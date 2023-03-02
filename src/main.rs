95ce28b052f2e34eb1c2213a2f9d8b9c16898ca4
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-03-02 16:30:45 +0545

    feat: New may of handling urls

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::fs::DirEntry;
-use std::path::PathBuf;
-use std::{env, fs};
+use std::collections::HashMap;
+use std::env;
+use std::fs::File;
+use std::path::{Path, PathBuf};
 
 use tiny_http::{Header, Request, Response, Server};
 
@@ -11,49 +12,52 @@ fn main() {
     });
     let path = PathBuf::from(path);
     debug_assert!(path.is_dir());
-    let dirs = path
-        .read_dir()
+    let urls_map = build_urls_map(path.as_path());
+
+    start_server(&urls_map);
+}
+
+fn build_urls_map(path: &Path) -> HashMap<String, PathBuf> {
+    let mut urls_map = HashMap::new();
+
+    path.read_dir()
         .unwrap()
         .into_iter()
         .map(|entry| entry.unwrap())
-        .collect::<Vec<DirEntry>>();
-    let index_page = get_index_page(&dirs);
+        .for_each(|dir_entry| {
+            if dir_entry.file_name() == "index.html" {
+                urls_map.insert(String::from("/"), dir_entry.path());
+                return;
+            }
+            urls_map.insert(
+                format!("/{}", dir_entry.file_name().to_str().unwrap()),
+                dir_entry.path(),
+            );
+        });
 
-    start_server(index_page);
+    urls_map
 }
 
-/// Returns a content of `index.html` file if given path contains it
-fn get_index_page(dirs: &[DirEntry]) -> Option<String> {
-    dirs.iter().find_map(|e| {
-        if e.file_name() == "index.html" {
-            Some(fs::read_to_string(e.path()).unwrap())
-        } else {
-            None
-        }
-    })
-}
-
-fn start_server(index_page: Option<String>) {
+fn start_server(urls_map: &HashMap<String, PathBuf>) {
     let server = Server::http("127.0.0.1:8080").unwrap();
     println!("Listening at `http://{}`", server.server_addr());
 
     for request in server.incoming_requests() {
-        handle_request(request, index_page.clone());
+        handle_request(request, urls_map);
     }
 }
 
-fn handle_request(request: Request, index_page: Option<String>) {
+fn handle_request(request: Request, urls_map: &HashMap<String, PathBuf>) {
     println!("{:?}: {}", request.method(), request.url());
 
-    if request.url() == "/" {
-        if let Some(index_page) = index_page {
-            let response = Response::from_string(index_page)
-                .with_header(Header::from_bytes("Content-Type", "text/html").unwrap());
-            request.respond(response).unwrap();
-            return;
-        }
+    if let Some(url_path) = urls_map.get(request.url()) {
+        let response = Response::from_file(File::open(url_path.as_path()).unwrap());
+        request.respond(response).unwrap();
+        return;
     }
 
-    let response = Response::from_string("Custom index.html file");
+    let response = Response::from_string("<h1>404 Not Found</h1>")
+        .with_header(Header::from_bytes("Content-Type", "text/html").unwrap())
+        .with_status_code(404);
     request.respond(response).unwrap();
 }
